f13d415c658046ffe66ad0438608b78ecd232252
sяva <slavik.bieriezhnoi1995@gmail.com>
2023-08-20 23:07:43 +0300

    new code

--- js/task-02.js
+++ js/task-02.js
@@ -1,13 +1,19 @@
-const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
+const ingredients = [
+	'Potatoes',
+	'Mushrooms',
+	'Garlic',
+	'Tomatos',
+	'Herbs',
+	'Condiments',
+ ];
+ const ingredientsRef = document.querySelector('#ingredients');
+ 
+ const itemsRef = ingredients.map((ingredient) => {
+	const item = document.createElement('li');
+	item.textContent = ingredient;
+	item.classList.add('item');
+	return item;
+ });
+ 
+ ingredientsRef.append(...itemsRef);
 
-const list = document.querySelector('#ingredients')
-const liArray = []
-
-ingredients.forEach(ingredient => {
-	const item = document.createElement('li')
-	item.className = 'item'
-	item.textContent = ingredient
-	liArray.push(item)
-})
-
-list.append(...liArray)
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,17 @@
-const inputChange = document.querySelector('input#font-size-control')
-const abracadabra = document.querySelector('span#text')
+// const inputChange = document.querySelector('input#font-size-control')
+// const abracadabra = document.querySelector('span#text')
 
-inputChange.addEventListener('change', e => {
-    abracadabra.style.fontSize = `${e.target.value }px`
-})
+// inputChange.addEventListener('change', e => {
+//     abracadabra.style.fontSize = `${e.target.value }px`
+// })
+
+
+const fontSizeControlInput = document.querySelector('#font-size-control');
+const textChange = document.querySelector('#text');
+
+fontSizeControlInput.addEventListener('input', onInputChange);
+
+function onInputChange({ target: { value } }) {
+  textChange.style.fontSize = `${value}px`;
+}
+
